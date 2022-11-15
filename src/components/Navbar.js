ae224af746d82dbd7caa022e33b1ec4ee45610b5
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2022-11-15 19:54:39 +0530

    Added hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,22 @@ function Navbar({ user }) {
               
             </Link>
           </div>
-          
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="menu-button"
+            className="h-6 w-6 cursor-pointer md:hidden block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="white"
+
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
           <div
             className="hidden w-full md:flex md:items-center md:w-auto"
             id="menu"
